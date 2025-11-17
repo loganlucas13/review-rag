1f0fe0b5e78c4dd30c6d5bf5de19bfecb46160de
Henry Chen <hchen250@uic.edu>
2025-11-16 21:22:52 -0600

    added vector embedding

--- app/backend/vector_db.py
+++ app/backend/vector_db.py
@@ -1,2 +1,3 @@
 # storing into vector database
 # phase 3 step 3
+
--- app/backend/vector_embedding.py
+++ app/backend/vector_embedding.py
@@ -1,3 +1,15 @@
 # vector embedding
 # phase 3 step 2
 import pgvector
+from typing import List
+import numpy as np
+from sentence_transformers import SentenceTransformer
+
+def vector_embedding(chunks: List[str]) -> np.ndarray:
+    model = SentenceTransformer("sentence_transformers/all-MiniLM-L6-v2")
+    emb_matrix = model.encode(chunks, convert_to_numpy=True, normalize_embeddings=True, batch_size=32)
+
+    return emb_matrix
+
+
+
